b119ecc02757687070c9d01a0f363637900c687d
ravinduKaushaly <ravinduk56@gmail.com>
2023-03-27 22:04:12 +0530

    stage setup

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,9 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -8,6 +12,11 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass()
+                .getResource("lk/ijse/D24_Room_Management_System/view/loginForm.fxml"))));
+        primaryStage.setTitle("Login Form");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 }
