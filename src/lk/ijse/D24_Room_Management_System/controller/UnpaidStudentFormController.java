f714b5eb264451a8ef558adaa9fcf3dae9eaf2c3
ravinduKaushaly <ravinduk56@gmail.com>
2023-04-15 00:06:27 +0530

    Designed unpaid StudentForm

--- src/lk/ijse/D24_Room_Management_System/controller/UnpaidStudentFormController.java
+++ src/lk/ijse/D24_Room_Management_System/controller/UnpaidStudentFormController.java
@@ -3,12 +3,27 @@ package lk.ijse.D24_Room_Management_System.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 
 public class UnpaidStudentFormController {
     public AnchorPane unpaidPane;
+    public Text txtId;
+    public Text txtName;
+    public Text txtAddress;
+    public Text txtContact;
+    public TableView tblStudent;
+    public TableColumn clmId;
+    public TableColumn clmName;
+    public TableColumn clmAddress;
+    public TableColumn clmContact;
+    public TableColumn clmDob;
+    public TableColumn clmGender;
+    public Text txtPaid;
 
     public void backOnAction(ActionEvent actionEvent) throws IOException {
         setUi("/lk/ijse/D24_Room_Management_System/view/studentForm.fxml");
@@ -18,5 +33,11 @@ public class UnpaidStudentFormController {
         Parent load = FXMLLoader.load(getClass().getResource(ui));
         unpaidPane.getChildren().clear();
         unpaidPane.getChildren().add(load);
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
     }
 }
