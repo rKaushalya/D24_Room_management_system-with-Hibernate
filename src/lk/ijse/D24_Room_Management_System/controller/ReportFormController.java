855dec7f2b08d737e5a75a2b8fcb3bae8b3c97ff
ravinduKaushaly <ravinduk56@gmail.com>
2023-04-22 00:15:11 +0530

    Bug Fixed

--- src/lk/ijse/D24_Room_Management_System/controller/ReportFormController.java
+++ src/lk/ijse/D24_Room_Management_System/controller/ReportFormController.java
@@ -1,4 +1,26 @@
 package lk.ijse.D24_Room_Management_System.controller;
 
+import javafx.event.ActionEvent;
+import lk.ijse.D24_Room_Management_System.util.FactoryConfiguration;
+import lk.ijse.D24_Room_Management_System.util.Utility;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.view.JasperViewer;
+
+import java.io.InputStream;
+import java.sql.Connection;
+
 public class ReportFormController {
+    public void loadEmployeeReportOnAction(ActionEvent actionEvent) {
+        try {
+            InputStream resource = this.getClass().getResourceAsStream("/lk/ijse/D24_Room_Management_System/view/report/studentReport.jrxml");
+
+            JasperReport jasperReport = JasperCompileManager.compileReport(resource);
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, (Connection) FactoryConfiguration.getInstance().getSession());
+
+            JasperViewer.viewReport(jasperPrint,false);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
